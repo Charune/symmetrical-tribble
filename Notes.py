601f7ffbd3efa4c7d9e8c88511559109b00d3604
charune <charune@protonmail.com>
2017-12-25 16:44:55 -0800

    Test Commit

--- Notes.py
+++ Notes.py
@@ -1,2 +1,3 @@
 #How to create a chance that a player injures themselves on Gameday
-#The injury would cause the match to be an automatic loss. 
+#The injury would cause the match to be an automatic loss.
+#Test
